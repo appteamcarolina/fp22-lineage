4891c9729062b96dd604bb3c280ebd91208c1238
dowden19 <dowden19@live.unc.edu>
2022-04-05 18:13:12 -0400

    Adding skeleton files

---
+++ fp22-lineage/Shared/fp22_lineageApp.swift
@@ -0,0 +1,17 @@
+//
+//  fp22_lineageApp.swift
+//  Shared
+//
+//  Created by Jackson Dowden on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct fp22_lineageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
